f2bb96f43a65fb9311f73b96d4fcfd0260d39527
Balaji Sawant <balaji.swt@gmail.com>
2023-05-02 12:41:48 +0530

    second largest no program added

--- Intersection & Union of two arrays/intersection.js
+++ Intersection & Union of two arrays/intersection.js
@@ -1,3 +1,26 @@
+/**
+ * Problem Statement:-
+ * Unique values only from 2 arrays i.e. intersection values
+const inputA = [1, 2, 3, 4, 5, 1];
+const inputB = [1, 2, 3, 4, 5, 1, 7, 6, 4, 3, 8];
+
+// Output
+[1, 2, 3, 4, 5,];
+* Algorithm:- 
+ *  1.create a function & take inputs array1 & array2 as the parametrs
+ *  2.As objects keys can be used to remove the redundant elements of the array 
+ *    hence traverse array1 elements through object using for loop.
+ *    2.1 For implemeting step 2 create empty object obj1.
+ *    2.2 by using obj.hasOwnProperty() push elements to obj1 & arr3.
+ * 3.Reapeat step 2 for array2 
+ *    3.1 For implemeting step 2 create empty object obj2.
+ * 4. After step 2 & 3 we have non redundant elements of array1 & array2 in 
+ *    array3 & obj2 keys. 
+ * 5. Now we will traverse over array3 & will check if the same element is 
+ *    present in obj2 keys if yes then push it to empty array arr4.
+ * 
+ */
+
 const intersectionOfTwoArrays = (arr1, arr2) => {
   let obj1 = {};
   let arr3 = [];
@@ -10,13 +33,14 @@ const intersectionOfTwoArrays = (arr1, arr2) => {
       arr3.push(arr1[i]);
     }
   }
-  console.log(arr3);
+
+  //   console.log('arr3=', arr3);
   for (let j = 0; j < arr2.length; j++) {
     if (!obj2.hasOwnProperty(arr2[j])) {
       obj2[arr2[j]] = j;
     }
   }
-  console.log(obj2);
+  //   console.log(obj2);
   for (let i = 0; i < arr3.length; i++) {
     if (obj2.hasOwnProperty(arr3[i])) {
       arr4.push(arr3[i]);
@@ -24,4 +48,4 @@ const intersectionOfTwoArrays = (arr1, arr2) => {
   }
   console.log(arr4);
 };
-intersectionOfTwoArrays([1, 2, 3, 4, 5, 6, 1, 9, 8, 10], [1, 2, 3, 4, 5, 1, 7, 6, 4, 3, 8]);
+intersectionOfTwoArrays([1, 2, 3, 4, 5, 1, 2, 10, 11, 12, 13, 14, 15], [1, 2, 3, 4, 5]);
--- Intersection & Union of two arrays/union.js
+++ Intersection & Union of two arrays/union.js
@@ -1,41 +1,62 @@
-const intersectionOfTwoArrays = (arr1, arr2) => {
-  let obj = {};
+/**
+ * Problem Statement:-Unique values only from 2 arrays i.e. union values
+ *   const inputA = [1, 2, 3, 4, 5, 1];
+ *   const inputB = [1, 2, 3, 4, 5, 1, 7, 6, 4, 3, 8];
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (!obj.hasOwnProperty(arr1[i])) {
-      obj[arr1[i]] = i;
-    }
-  }
-  for (let j = 0; j < arr2.length; j++) {
-    if (!obj.hasOwnProperty(arr2[j])) {
-      obj[arr2[j]] = j;
-    }
-  }
+ *   // Output
+ *   [1, 2, 3, 4, 5, 6, 7, 8];
 
-  return Object.keys(obj);
-};
-console.log(intersectionOfTwoArrays([1, 2, 3, 4, 5, 1, 9, 10], [1, 2, 3, 4, 5, 1, 7, 6, 4, 3, 8]));
+ *  Algorithm:-
+ *  1.create a function & take inputs array1 & array2 as the parametrs
+ *  2.As objects keys can be used to remove the redundant elements of the array 
+ *    hence traverse array1 elements through object using for loop.
+ *    2.1 For implemeting step 2 create empty object obj.
+ *    2.2 by using obj.hasOwnProperty() push elements to obj as keys.
+ *  3.Repeat the step 2 over array2 to get the non redundant elements from the
+ *    array2 & push them on obj to get the union of two arrays.
+ */
 
-// const intersectionOfTwoArraysV2 = (arr1, arr2) => {
+// const intersectionOfTwoArrays = (arr1, arr2) => {
 //   let obj = {};
 
-//   for (let i = 0, j = 0; i < arr1.length, j < arr2.length; i++, j++) {
-//     if (!obj.hasOwnProperty(arr1[i]) && !obj.hasOwnProperty(undefined)) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (!obj.hasOwnProperty(arr1[i])) {
 //       obj[arr1[i]] = i;
 //     }
-
-//     //   console.log(obj);
-
-//     if (!obj.hasOwnProperty(arr2[j]) && !obj.hasOwnProperty(undefined)) {
+//   }
+//   for (let j = 0; j < arr2.length; j++) {
+//     if (!obj.hasOwnProperty(arr2[j])) {
 //       obj[arr2[j]] = j;
 //     }
 //   }
 
 //   return Object.keys(obj);
 // };
-// console.log(
-//   intersectionOfTwoArraysV2(
-//     [1, 2, 3, 4, 5, 1, 9, 10],
-//     [1, 2, 3, 4, 5, 1, 7, 6, 4, 3, 8]
-//   )
-// );
+// console.log(intersectionOfTwoArrays([1, 2, 3], [4, 5, 1, 7, 6, 4, 3, 8]));
+
+const intersectionOfTwoArraysV2 = (arr1, arr2) => {
+  let obj = {};
+  let l1 = arr1.length;
+  let l2 = arr2.length;
+  if (l1 > l2) {
+    l2 = l2 + l1 - l2;
+  } else {
+    l1 = l1 + l2 - l1;
+  }
+  console.log(l1, l2);
+
+  for (let i = 0, j = 0; i < l1, j < l2; i++, j++) {
+    if (!obj.hasOwnProperty(arr1[i]) && !obj.hasOwnProperty(undefined)) {
+      obj[arr1[i]] = i;
+    }
+
+    console.log(obj);
+
+    if (!obj.hasOwnProperty(arr2[j]) && !obj.hasOwnProperty(undefined)) {
+      obj[arr2[j]] = j;
+    }
+  }
+
+  return Object.keys(obj);
+};
+console.log(intersectionOfTwoArraysV2([1, 2, 3, 4], [4, 5, 6, 7]));
